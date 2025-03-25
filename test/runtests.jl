4e9b7df1a11f85c54094fe39e8d688fa861011c3
abhro <5664668+abhro@users.noreply.github.com>
2025-03-25 11:48:40 -0400

    Add aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,15 @@
 using MagneticFieldSHTCExpander
 using Test
 using OffsetArrays
+using Aqua
 
-@testset "MagneticFieldSHTCExpander.jl" begin
+@testset "MagneticFieldSHTCExpander" begin
 
     zerog = zeros(0:12, 0:12)
     zeroh = zeros(0:12, 0:12)
     @test magneticfield(1.4, π/3, 0.8*π, zerog, zeroh).Φ == 0
+
+    @testset "Code quality (Aqua)" begin
+        Aqua.test_all(MagneticFieldSHTCExpander)
+    end
 end
